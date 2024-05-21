f90195b51684e2bee15f0398bfeec96e799ae554
Vlasov R. V <radislavvlasov@gmail.com>
2024-05-21 14:40:09 +0300

    feat: добавлена плашка навигации

---
+++ app.py
@@ -0,0 +1,29 @@
+import dash.development.base_component
+import dash_bootstrap_components as dbc
+from dash import Dash
+
+
+def construct_app_layout() -> dbc.Container:
+    navigation_bar = dbc.NavbarSimple(
+        [dbc.NavItem(dbc.NavLink(page["name"], href = page["path"])) for page in dash.page_registry.values()],
+    )
+
+    layout = dbc.Container(
+        [
+            navigation_bar,
+            dash.page_container
+        ]
+    )
+    return layout
+
+
+def run() -> None:
+    app = Dash(use_pages = True)
+
+    app.layout = construct_app_layout()
+
+    app.run(debug = True)
+
+
+if __name__ == "__main__":
+    run()
--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,7 @@ import dash
 from dash import html
 
 
-dash.register_page(__name__, path = "/")
+dash.register_page(__name__, "/")
 
 layout = html.Div(
     [
